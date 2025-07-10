48e91746cd8a0734358f8d1b55c4a7231e1bf6a3
Muhammad Saleem Shuaibu <shuaibusalim@gmail.com>
2025-07-10 21:14:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { DashboardHeader } from '@/components/dashboard-header';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function DashboardLayout({
   children,
@@ -8,11 +9,13 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <DashboardHeader />
-      <main className="flex-1">
-        {children}
-      </main>
-    </div>
+    <SidebarProvider>
+      <div className="flex flex-col min-h-screen">
+        <DashboardHeader />
+        <main className="flex-1">
+          {children}
+        </main>
+      </div>
+    </SidebarProvider>
   );
 }
